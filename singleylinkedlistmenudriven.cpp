cd71d31417491b0f201df481164b07814f62a344
46Ashish1 <120231235+46Ashish1@users.noreply.github.com>
2022-12-19 07:42:28 +0530

    Update singleylinkedlistmenudriven.cpp

--- singleylinkedlistmenudriven.cpp
+++ singleylinkedlistmenudriven.cpp
@@ -1,375 +1,186 @@
 #include <iostream>
 using namespace std;
-template <class T>
 class Node
 {
 public:
-    T info;
-    Node *ptr;
+    int data;
+    Node *next;
 };
-template <class T>
-class SinglyLinkedList
+void printList(Node *n)
 {
-protected:
-    Node<T> *head, *tail;
+    while (n != NULL)
+    {
+        cout << n->data << " ";
+        n = n->next;
+    }
+    cout << endl;
+}
+void insertBeginning(Node *head)
+{
+    int x;
+    cout << "Enter the value to insert at beginning : ";
 
-public:
-    // Constructor
-    SinglyLinkedList()
+    cin >> x;
+    Node *temp = new Node();
+    temp->data = x;
+    temp->next = head;
+    head = temp;
+    printList(head);
+}
+void insertPosition(Node *head)
+{
+    int x;
+    int pos;
+    cout << "Enter the element to be inserted ; ";
+    cin >> x;
+    cout << "Enter the position to insert in : ";
+    cin >> pos;
+    Node *nyu = new Node();
+    nyu->data = x;
+    Node *n = head;
+    Node *temp = NULL;
+    for (int i = 0; i < pos; i++)
     {
-        head = tail = NULL;
+        temp = n;
+        n = n->next;
     }
-    // Destructor
-    ~SinglyLinkedList()
-    {
-        if (this->isEmpty())
-            return;
-Node<T> *ptr, *temp = head;
-while (temp != NULL)
-{
-    ptr = temp->ptr;
-    delete temp;
-    temp = ptr;
+    nyu->next = n;
+    temp->next = nyu;
+    printList(head);
 }
-head = tail = NULL;
-return;
+void deleteBeginning(Node *head)
+{
+    Node *temp = head;
+    head = head->next;
+    free(temp);
+    printList(head);
+}
+void deletePosition(Node *head)
+{
+    int pos;
+    cout << "Enter the positon to delete from : ";
+    cin >> pos;
+    Node *n = head;
+    Node *prev = NULL;
+    for (int i = 0; i < pos; i++)
+    {
+        prev = n;
+        n = n->next;
     }
-    // Checks if the list is empty - O(1)
-    bool isEmpty()
+    Node *temp = n;
+    prev->next = temp->next;
+    free(temp);
+    printList(head);
+}
+Node *searchElement(Node *head)
+{
+    int x;
+    cout << "Enter the element to find for : ";
+
+    cin >> x;
+    Node *n = head;
+    while (n->data != x)
     {
-return (head == NULL || tail == NULL);
+        n = n->next;
     }
-    // Inserts a node at the beginning - O(1)
-    void insertFront(T info)
+    return n;
+}
+void concatenate(Node *head, Node *head2)
+{
+    Node *n = head;
+    Node *prev = NULL;
+    while (n != NULL)
     {
-Node<T> *temp = new Node<T>();
-temp->info = info;
-temp->ptr = head;
-if (this->isEmpty())
-    tail = temp;
-head = temp;
-cout << "Inserted " << info << " at front...";
-this->display();
-return;
+        prev = n;
+        n = n->next;
     }
-    // Inserts a node at a specified location - O(n)
-    void insertAtLoc(int loc, T info)
+    prev->next = head2;
+    printList(head);
+}
+int main()
+{
+    // linked list 1
+    Node *head = NULL;
+    Node *second = NULL;
+    Node *third = NULL;
+    Node *fourth = NULL;
+    Node *fifth = NULL;
+    head = new Node();
+    second = new Node();
+    third = new Node();
+    fourth = new Node();
+    fifth = new Node();
+    head->data = 1;
+    head->next = second;
+    second->data = 2;
+    second->next = third;
+    third->data = 3;
+    third->next = fourth;
+
+    fourth->data = 4;
+    fourth->next = fifth;
+    fifth->data = 5;
+    fifth->next = NULL;
+
+    // linked list 2
+    Node *list1 = new Node();
+    Node *list2 = new Node();
+    Node *list3 = new Node();
+
+    list1->data = 6;
+
+    list1->next = list2;
+
+    list2->data = 7;
+    list2->next = list3;
+
+    list3->data = 8;
+    list3->next = NULL;
+
+    printList(head);
+    int choice = 1;
+    while (choice != 0)
     {
-if (loc == 1)
-{
-    this->insertFront(info);
-    return;
-}
-Node<T> *temp = head;
-for (int i = 1; temp != NULL && i < loc - 1; i++)
-    temp = temp->ptr;
-if (temp == NULL)
-{
-    cout << "Invalid location...\n";
-    return;
-}
-if (temp == tail)
-{
-    this->insertBack(info);
-    return;
-}
-Node<T> *node = new Node<T>();
-node->info = info;
-node->ptr = temp->ptr;
-temp->ptr = node;
-cout << "Inserted node " << info << " at location " << loc << "...";
-this->display();
-return;
-    }
-    // Inserts a node at the end - O(1)
-    void insertBack(T info)
-    {
-Node<T> *temp = new Node<T>();
-temp->info = info;
-temp->ptr = NULL;
-if (this->isEmpty())
-    head = tail = temp;
-else
-    tail->ptr = temp;
-tail = temp;
-cout << "Inserted " << info << " at back...";
-this->display();
-return;
-    }
-    // Removes a node from the beginning - O(1)
-    void deleteFront()
-    {
-if (this->isEmpty())
-{
-    cout << "\nList is empty...\n";
-    return;
-}
-Node<T> *temp = head;
-head = temp->ptr;
-delete temp;
-if (this->isEmpty())
-    tail = NULL;
-cout << "\nDeleted node at front...";
-this->display();
-return;
-    }
-    // Removes a node at a specified location - O(n)
-    void deleteAtLoc(int loc)
-    {
-if (this->isEmpty())
-{
-    cout << "\nList is empty...\n";
-    return;
-}
-if (loc == 1)
-{
-    this->deleteFront();
-    return;
-}
-Node<T> *node, *temp = head;
-for (int i = 1; temp != NULL && i < loc - 1; i++)
-    temp = temp->ptr;
-if (temp == NULL || temp->ptr == NULL)
-{
-    cout << "Invalid location...\n";
-    return;
-}
-if (temp == tail)
-{
-    this->deleteBack();
-    return;
-}
-node = temp->ptr->ptr;
-delete temp->ptr;
-temp->ptr = node;
-cout << "Deleted node "
-     << "at location " << loc << "...";
-this->display();
-return;
-    }
-    // Removes a node at the end - O(n)
-    void deleteBack()
-    {
-if (this->isEmpty())
-{
-    cout << "\nList is empty...\n";
-    return;
-}
-if (head == tail)
-{
-    this->deleteFront();
-    return;
-}
-else
-{
-    Node<T> *temp = head;
-    while (temp->ptr->ptr != NULL)
-        temp = temp->ptr;
-    delete temp->ptr;
-    temp->ptr = NULL;
-    tail = temp;
-}
-cout << "\nDeleted node at back...";
-this->display();
-return;
-    }
-    // Reverses the linked list - O(n)
-    void reverse()
-    {
-if (this->isEmpty())
-{
-    cout << "\nList is empty...\n";
-    return;
-}
-Node<T> *temp = head,
-        *prev = NULL,
-        *next = NULL;
-tail = temp;
-while (temp != NULL)
-{
-    next = temp->ptr;
-    temp->ptr = prev;
-    prev = temp;
-    temp = next;
-}
-head = prev;
-cout << "\nList reversed...";
-this->display();
-return;
-    }
-    // Concatenates two lists - O(n)
-    void concat(SinglyLinkedList<T> &list)
-    {
-if (!list.isEmpty() && !this->isEmpty())
-{
-    Node<T> *node,
-        *temp = tail,
-        *temp1 = list.head;
-    while (temp1 != NULL)
-    {
-        node = new Node<T>();
-        node->info = temp1->info;
-        node->ptr = NULL;
-        temp->ptr = node;
-        temp = temp->ptr;
-        temp1 = temp1->ptr;
-    }
-    tail = node;
-    cout << "Concatenated two lists...\n";
-    this->display();
-}
-else
-    cout << "\nOne of the lists is empty...\n";
-return;
-    }
-    // Overloads the + operator - O(n)
-    void operator+(SinglyLinkedList<T> &list)
-    {
-this->concat(list);
-return;
-    }
-    // Searches for an element - O(n)
-    Node<T> *search(T ele)
-    {
-if (this->isEmpty())
-    return nullptr;
-Node<T> *temp = head;
-while (temp != NULL)
-{
-    if (temp->info == ele)
-        return temp;
-    temp = temp->ptr;
-}
-return nullptr;
-    }
-    // Calculates the number of nodes - O(n)
-    int count()
-    {
-if (this->isEmpty())
-{
-    cout << "\nList is empty...\n";
-    return -1;
-}
-int count = 0;
-Node<T> *temp;
-for (temp = head; temp != NULL; temp = temp->ptr, count++)
-    ;
-return count;
-    }
-    void getch()
-    {
-cout << "\nPress any key to continue...";
-cin.ignore();
-cin.get();
-return;
-    }
-    // Traverses the list and prints all nodes - O(n)
-    void display()
-    {
-if (this->isEmpty())
-{
-    cout << "\nList is empty...\n";
-    return;
-}
-Node<T> *temp = head;
-cout << "\nList: ";
-while (temp->ptr != NULL)
-{
-    cout << temp->info << " -> ";
-    temp = temp->ptr;
-}
-cout << temp->info << endl;
-return;
-    }
-};
-int main(void)
-{
-    int choice, ele, info, loc, count;
-    SinglyLinkedList<int> list, list2;
-    do
-    {
-cout << "\tSingly Linked List\n"
-     << "===================================\n"
-     << " (1) Search (2) InsertFront\n"
-     << " (3) InsertBack (4) InsertAtLoc\n"
-     << " (5) DeleteFront (6) DeleteBack\n"
-     << " (7) DeleteAtLoc (8) Display\n"
-     << " (9) Count (10) Reverse\n"
-     << " (11) Concat (0) Exit\n\n";
-cout << "Enter Choice: ";
-cin >> choice;
-switch (choice)
-{
-case 1:
-    cout << "\nEnter Search Element: ";
-    cin >> ele;
-    if (list.search(ele) != nullptr)
-        cout << "Element " << ele << " found...\n";
-    else
-        cout << "Element not found or List is Empty...\n";
-    break;
-case 2:
-    cout << "\nEnter Element: ";
-    cin >> info;
-    list.insertFront(info);
-    break;
-case 3:
-    cout << "\nEnter Element: ";
-    cin >> info;
-    list.insertBack(info);
-    break;
-case 4:
-    cout << "\nEnter Location: ";
-    cin >> loc;
-    cout << "Enter Element: ";
-    cin >> info;
-    list.insertAtLoc(loc, info);
-    break;
-case 5:
-    list.deleteFront();
-    break;
-case 6:
-    list.deleteBack();
-    break;
-case 7:
-    cout << "\nEnter Location: ";
-    cin >> loc;
-    list.deleteAtLoc(loc);
-    break;
-case 8:
-    list.display();
-    break;
-case 9:
-    count = list.count();
-    if (count != -1)
-        cout << "\nNumber of Nodes: " << count;
-    break;
-case 11:
-    if (!list2.isEmpty())
-    {
-        cout << "\nList B:";
-        list2.display();
-    }
-    cout << "\nNumber of Nodes to add in List B: ";
-    cin >> count;
-    if (count)
-    {
-        cout << "Enter Elements to List B: ";
-        for (int i = 0; i < count; i++)
+        cout << "1. Insert an element x at the beginning of the singly linked list "<<endl;
+        cout<< "2. Insert an element x at ith position in the singly linked list "<<endl;
+        cout<< "3. Remove an element from the beginning of the singly linked list "<<endl;
+        cout<< "4. Remove an element from ith position in the singly linked list."<<endl;
+        cout<< "5. Search for an element x in the singly linked list and return its pointer "<<endl;
+        cout<< "6. Concatenate two singly linked lists" << endl;
+        cout << "Enter 0 to Exit " << endl;
+        cout << "Enter choice : ";
+        cin >> choice;
+        switch (choice)
         {
-            cin >> info;
-            list2.insertBack(info);
+        case 1:
+            insertBeginning(head);
+            break;
+        case 2:
+            insertPosition(head);
+            break;
+        case 3:
+            deleteBeginning(head);
+            break;
+        case 4:
+            deletePosition(head);
+            break;
+        case 5:
+        {
+            Node *ans = searchElement(head);
+            cout << "Element found at : " << ans << endl;
+            break;
         }
-        list + list2;
+        case 6:
+        {
+            concatenate(head, list1);
+            break;
+        }
+        default:
+        {
+            cout << "Enter valid option : " << endl;
+            break;
+        }
+        }
     }
-    break;
-case 10:
-    list.reverse();
-    break;
-case 0:
-default:
-    break;
-}
-    } while (choice != 0);
     return 0;
 }
